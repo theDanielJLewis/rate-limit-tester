87bd597faab7378f76d84fcee49f386024b17245
Daniel J. Lewis <mail@TheAudacitytoPodcast.com>
2019-12-14 00:56:48 -0500

    Potential refactor to allow progress concurrency

--- index.js
+++ index.js
@@ -4,68 +4,151 @@ const async = require('async');
 const ProgressBar = require('progress');
 const argv = require('yargs')
     .usage('Usage: node $0 --url=[URL] --max=[NUM] [OPTIONS]')
-    .describe('url', 'URL to test, best to be in quotation marks')
-    .describe('max', 'Maximum requests to run per time')
-    .describe('min', 'Minimum requests to run per time')
-    .describe('s', 'Set to requests to second [default]')
-    .describe('m', 'Set to requests to minute')
-    .alias('u', 'url')
+    .option({
+        'u': {
+            alias: 'url',
+            describe: 'URL to test, best to be in quotation marks',
+            demandOption: true,
+        },
+        'per': {
+            describe: 'Set measurement basis',
+            choices: ['second','minute'],    
+        },
+        't': {
+            alias: 'test',
+            describe: 'What to test and measure, defaults to persecond',
+            choices: ['concurrent', 'persecond'],
+        }
+    })
+    .describe('min', 'Minimum value for tests')
+    .describe('max', 'Maximum value for tests')
+    .describe('concurrent', 'Number of concurrent requests')
     .alias('h', 'help')
-    .demandOption(['url','max'])
+    .demandOption(['max'])
     .argv;
 
-function testRateLimit() {
+const url = argv.url;
+const max = argv.max || 20;
+const min = argv.min || max;
+const reqOptions = {
+    simple: true,
+    resolveWithFullResponse: true,
+    time: true,
+}
+let perTime;
+let testSettings;
+const perSecond = {
+    seconds: 1,
+    label: 'per second',
+};
+const perMinute = {
+    seconds: 60,
+    label: 'per minute',
+}
 
-    let secOrMin = 1; // Default to checking per second
-    if (argv.s) secOrMin = 1;
-    if (argv.m) secOrMin = 60;
 
-    const url = argv.url;
-    const max = argv.max || 20;
-    const min = argv.min || max;
-    const maxConcurrent = argv.concurrent || 1;
-    const reqOptions = {
-        simple: true,
-        resolveWithFullResponse: true,
-        time: true,
+switch (argv.per) {
+    case 'second':
+        perTime = perSecond;
+        break;
+
+    case 'minute':
+        perTime = perMinute;
+        break;
+    
+    default:
+        perTime = perSecond;
+        break;
+}
+
+prepTest();
+
+async function prepTest() {
+    try {
+        const speedtest = await rp(url, reqOptions);
+        // var speedtestTiming = speedtest.timingPhases.total; // Pretend it's 500 ms
+        var speedtestTiming = 150; // Pretend it's 500 ms
+        console.log(speedtestTiming, 'ms per run');
+        runTest(speedtestTiming);
+    } catch (error) {
+        console.error(error);
     }
+}
+
+
+function runTest(speedtestTiming) {
 
     // How many different rate limits to test
     const limitsToTest = max - min + 1;
-    console.log(limitsToTest, 'limits to test:', min, 'to', max, 'per time');
+
+    console.log(limitsToTest, 'limits to test:', min, 'to', max, perTime.label);
+
     async.timesLimit(limitsToTest, 1, function(limitTestNum, nextTest) {
 
         // The limit to test
-        const testLimit = min + limitTestNum;
+        testLimit = min + limitTestNum;
 
-        const limiter = new Bottleneck({
-            reservoir: testLimit, // initial value
-            reservoirRefreshAmount: testLimit,
-            reservoirRefreshInterval: secOrMin * 1000, // must be divisible by 250
-            // also use maxConcurrent and/or minTime for safety
-            maxConcurrent,
-            // minTime: 333 // pick a value that makes sense for your use case
-        });
+        if (argv.test == 'concurrent') {
+
+            limiter = new Bottleneck({
+                maxConcurrent: testLimit
+            });
+
+            timesPerSecond = speedtestTiming / 1000;
+            // console.log(timesPerSecond);
+            // if (timesPerSecond > max) timesPerSecond = max;
+            // console.log(timesPerSecond);
+            timesPerMinute = (60 / timesPerSecond) * testLimit;
+            // console.log(timesPerMinute);
+
+        } else if (argv.test == 'persecond') {
+    
+            limiter = new Bottleneck({
+                reservoir: testLimit, // initial value
+                reservoirRefreshAmount: testLimit,
+                reservoirRefreshInterval: perTime.seconds * 1000, // must be divisible by 250
+                maxConcurrent: argv.concurrent || 1,
+            });
+
+            timesPerSecond = (1000 / speedtestTiming);
+            // console.log(timesPerSecond);
+            if (timesPerSecond > testLimit) timesPerSecond = testLimit;
+            // console.log(timesPerSecond);
+            timesPerMinute = 60 * timesPerSecond;
+
+        }
 
         // Test response time to figure out how many requests to send in a minute
         rp(url, reqOptions)
             .then( (speedtest) => {
-                var speedtestTiming = speedtest.timingPhases.total;
-                timesPerSecond = (1000 / speedtestTiming);
-                // console.log(timesPerSecond);
-                if (timesPerSecond > max) timesPerSecond = max;
-                // console.log(timesPerSecond);
+                // var speedtestTiming = speedtest.timingPhases.total; // Pretend it's 500 ms
+                var speedtestTiming = 150; // Pretend it's 500 ms
+                console.log(speedtestTiming, 'ms per run');
+                if (argv.test == 'concurrent') {
+                    timesPerSecond = speedtestTiming / 1000;
+                    // console.log(timesPerSecond);
+                    // if (timesPerSecond > max) timesPerSecond = max;
+                    // console.log(timesPerSecond);
+                    timesPerMinute = (60 / timesPerSecond) * testLimit;
+                    // console.log(timesPerMinute);
+                } else if (argv.test == 'persecond') {
+                    timesPerSecond = (1000 / speedtestTiming);
+                    // console.log(timesPerSecond);
+                    if (timesPerSecond > testLimit) timesPerSecond = testLimit;
+                    // console.log(timesPerSecond);
+                    timesPerMinute = 60 * timesPerSecond;
+                }
             })
             .then( function() {
                 // Run a minute's worth of tests
-                const timesToRun = Math.round(60 * timesPerSecond);
+                const timesToRun = Math.round(timesPerMinute);
                 console.log(testLimit, 'per time in', timesToRun, 'runs');
                 var bar = new ProgressBar('|:bar| :percent :elapsed', { 
                     total: timesToRun,
                     // clear: true
                     width: 40,
                 });
-                async.timesLimit(timesToRun, 1, (runNum, nextRun) => {
+                async.times(timesToRun, (runNum, nextRun) => {
                     // console.log('run', runNum, 'for', testLimit, 'per time');
                     limiter.schedule(() => rp(url, reqOptions))
                         .then((result) => {
@@ -96,6 +179,4 @@ function testRateLimit() {
         if (error) console.log(error);
     });
 
-}    
-
-testRateLimit();
+}
